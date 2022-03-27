7d30e86d2acb21d99996ff6d3d25cf2b30dce738
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2022-03-27 18:42:06 +0200

    Update NumericalBond's test

--- src/test/NumericalBondTest.java
+++ src/test/NumericalBondTest.java
@@ -4,11 +4,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class NumericalBondTest {
+import main.general.GameController;
+import main.numericalbond.NumericalBondController;
+import main.numericalbond.model.Block;
+import main.numericalbond.model.Direction;
 
+class NumericalBondTest {
+	
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		final GameController numericalBondController = new NumericalBondController();
+		numericalBondController.start();
+		//fail("Not yet implemented");
+		
+	}
+	
+	@Test
+	void blockTest() {
+		final Block block = new Block(5);
+		assertTrue(block.getMaxLinks() == 5);
+		block.addLink(Direction.UP);
+		assertTrue(block.getCurrentLinks() == 1);
+		block.addLink(Direction.DOWN);
+		block.addLink(Direction.UP);
+		assertTrue(block.getCurrentLinks() == 3);
 	}
 
 }
