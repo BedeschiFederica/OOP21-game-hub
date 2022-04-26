c81387727d4544038dc64c749091e8bfdcce7d35
Silvia Furegato <silvia.furegato@studio.unibo.it>
2022-04-26 20:16:46 +0200

    Edit FloodItView's style

--- src/main/games/floodit/view/FloodItView.java
+++ src/main/games/floodit/view/FloodItView.java
@@ -1,14 +1,11 @@
 package main.games.floodit.view;
 
-import javax.swing.*;
-
-import main.dashboard.view.PauseMenu;
 import main.games.floodit.controller.FloodItController;
 import main.games.floodit.model.Cell;
-import main.games.floodit.model.FloodItModel;
 import main.games.floodit.model.Table;
 import main.general.GameView;
 
+import javax.swing.*;
 import java.util.*;
 import java.util.List;
 import java.awt.*;
@@ -16,6 +13,10 @@ import java.awt.*;
 public class FloodItView implements GameView {
 
     private static final long serialVersionUID = -6218820567019985015L;
+    private static final int SIZE_DIV = 2;
+    private static final int MIN_WIDTH = 400;
+    private static final int MIN_HEIGHT = 500;
+
     private final List<JButton> cellButtons = new ArrayList<>();
     private final Map<JButton, Cell> cellsMap;
 
@@ -29,10 +30,10 @@ public class FloodItView implements GameView {
     private final StartPanel startPanel;
     private final JPanel pausePanel;
 
-    public FloodItView(FloodItController controller) {
+    public FloodItView(final FloodItController controller) {
         this.frame = new JFrame();
         this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.frame.setMinimumSize(new Dimension(400, 500));
+        this.frame.setMinimumSize(new Dimension(MIN_WIDTH, MIN_HEIGHT));
 
         this.cellsMap = new HashMap<>();
         this.controller = controller;
@@ -44,19 +45,19 @@ public class FloodItView implements GameView {
 
         this.startPanel = new StartPanel(mainPanel, layout, controller);
         this.gamePanel = null;
-        this.pausePanel = new JPanel(); //new PausePanel(mainPanel, layout);
+        this.pausePanel = new JPanel();
 
         this.mainPanel.add(startPanel, "1");
         this.mainPanel.add(pausePanel, "3");
 
-        showStart();
+        this.layout.show(mainPanel, "1");
 
     }
 
     public void display() {
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         final int sw = (int) screen.getWidth();
-        this.frame.setSize(sw / 2, sw / 2);
+        this.frame.setSize(sw / SIZE_DIV, sw / SIZE_DIV);
         this.frame.setLocationByPlatform(true);
         this.frame.setVisible(true);
     }
@@ -95,12 +96,12 @@ public class FloodItView implements GameView {
     }
 
     @Override
-    public void setVisible(boolean visible) {
+    public void setVisible(final boolean visible) {
         if (visible) {
             layout.show(mainPanel, "2");
         }
     }
-    
+
     public void showStart() {
         this.layout.show(mainPanel, "1");
     }
